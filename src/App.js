80c84bb33018a35181062fc9b6ccc37581d2d2c2
Rohit Bhirud <rohit.3mindsdigital@gmail.com>
2018-02-01 13:42:47 +0530

    state with lifecycle

--- src/App.js
+++ src/App.js
@@ -12,16 +12,25 @@ class App extends React.Component {
     }
     componentWillMount() {
         console.log('componentWillMount');
+        this.setState({m: 2})
     }
+
+    render() {
+        console.log('render');
+        return (
+            <button onClick={this.update}>
+                {this.state.val * this.state.m}
+            </button>
+        )
+    }
+
     componentDidMount() {
         console.log('componentDidMount');
+        this.inc = setInterval(this.update, 500)
     }
     componentWillUnmount() {
         console.log('componentWillUnmount');
-    }
-    render() {
-        console.log('render');
-        return <button onClick={this.update}>{this.state.val}</button>
+        clearInterval(this.inc)
     }
 }
 

1e2f431ecb856496a818d83ee208e40ba4133b4a
Rohit Bhirud <rohit.3mindsdigital@gmail.com>
2018-02-01 12:41:43 +0530

    state management

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 class App extends React.Component {
-    render() {
-        let txt = this.props.txt
-        return <h1>{txt}</h1>
+    constructor() {
+        super();
+        this.state = {
+            txt: 'this is a state txt',
+            cat: 0
+        }
     }
-}
-
-App.propTypes = {
-    txt: PropTypes.string,
-    cat: PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-    txt: "this is the default txt"
+    update(e) {
+        this.setState({txt: e.target.value})
+    }
+    render() {
+        return (
+            <div>
+                <input type="text"
+                    onChange={this.update.bind(this)} />
+                <h1>{this.state.txt} - {this.state.cat}</h1>
+            </div>
+        )
+    }
 }
 
 export default App
